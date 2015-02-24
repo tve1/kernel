77d47a754a787b75aa50951e50b7f00b62804178
David Alexander Nichol <dan1@glass.clear.rice.edu>
2015-02-23 23:01:33 -0600

    changed physical_pages from linked list to array

--- yalnix.c
+++ yalnix.c
@@ -10,11 +10,11 @@
 void* ivt[TRAP_VECTOR_SIZE];
 
 struct node {
-  struct node* nextNode;
   void* base;
 };
 
-struct node* physical_pages;
+struct node** physical_pages;
+int physical_pages_length;
 
 void trapKernel(ExceptionStackFrame *frame){
 	TracePrintf(0, "trapKernel");
@@ -45,6 +45,25 @@ void trapTtyTransmit(ExceptionStackFrame *frame){
 	Halt();
 }
 
+// void * allocPhysicalPage(){
+// 	if (physical_pages == NULL){
+// 		return NULL;
+// 	}
+// 	void * physicalPageAddr = physical_pages->base;
+// 	struct node * tempNode = physical_pages->nextNode;
+// 	free(physical_pages);
+// 	physical_pages = tempNode;
+// 	return physicalPageAddr;
+// }
+
+// int freePhysicalPage(void * addr) {
+// 	struct node* new_page = malloc(sizeof(struct node*));
+// 	new_page->nextNode = physical_pages;
+// 	new_page->base = addr;
+// 	physical_pages = new_page;
+// 	return 1;
+// }
+
 void KernelStart (ExceptionStackFrame *frame, unsigned int pmem_size, void *orig_brk, char **cmd_args) {
 	void (*ptr_to_kernel)(ExceptionStackFrame *);
 	ptr_to_kernel = &trapKernel;
@@ -71,20 +90,37 @@ void KernelStart (ExceptionStackFrame *frame, unsigned int pmem_size, void *orig
 	WriteRegister(REG_VECTOR_BASE, (RCS421RegVal) ivt);
 
 	long current_spot = (long) orig_brk;
-	struct node* old_page = NULL;
-	while (current_spot + PAGESIZE < pmem_size) {
+	
+	int num_nodes = (pmem_size - current_spot) / PAGESIZE;
+	physical_pages = malloc(sizeof(struct node*) * num_nodes);
+	int i;
+
+	for (i = 0; i < num_nodes; i++) {
 		struct node* new_page = malloc(sizeof(struct node*));
-		new_page->nextNode = old_page;
-		new_page->base = current_spot;
-		old_page = new_page;
-		current_spot += PAGESIZE;
-		printf("%p -> %p\n", new_page, new_page->nextNode);
+		physical_pages[i] = new_page;
 	}
+
+	current_spot = (long) &_etext;
+
+	int index = 0;
+	
+	// Only going one-direction
+	while (current_spot + PAGESIZE < pmem_size) {
+		physical_pages[index]->base = (void*)current_spot;
+		current_spot += PAGESIZE;
+		index++;
+	}
+	physical_pages_length = index;
 }
+
+
+
+
 
 int SetKernelBrk(void *addr){
 	printf("Brk\n");
 	TracePrintf(0,"kernel break");
+	_etext = (long)addr;
 	return 0;
 }
 
